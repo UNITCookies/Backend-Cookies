4acf9bb03c710c9de1fd64832ca8ba064e307681
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-09-03 15:21:26 +0900

    [Feat] Base Class 추가

---
+++ src/main/java/com/letter/cookies/exception/BaseException.java
@@ -0,0 +1,14 @@
+package com.letter.cookies.exception;
+
+import com.letter.cookies.dto.response.CustomResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseException extends Exception {
+
+    private CustomResponseStatus status;
+}
